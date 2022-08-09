87258c924ae3b4154dc1eb0e3fd0c89ad05feb59
LinEvil <13027656+LinEvil@users.noreply.github.com>
2022-08-09 15:03:39 +0800

    update error-handler.js

--- app/middleware/error-handler.js
+++ app/middleware/error-handler.js
@@ -2,8 +2,10 @@ const util = require("util");
 
 module.exports = () => {
   return (err, req, res, next) => {
+    console.log(err);
     res.status(500).json({
-      error: util.format(err),
+      code: "500",
+      message: "服务器错误"
     });
   };
 };
