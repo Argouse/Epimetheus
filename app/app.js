6a27e170c7b5e99a4402cdc2a6e31db89712b74f
iiijr <908809979@qq.com>
2022-08-09 14:22:19 +0800

    add cors

--- app/app.js
+++ app/app.js
@@ -7,14 +7,26 @@ const app = express()
 
 const PORT = process.env.PORT || 3030
 
+const cors = require("cors")
+
+const corsOptions = {
+    credentials: true, //允许客户端携带验证信息
+    origin: (origin, callback) => {
+        if (origin)
+            return callback(null, true)
+ 
+        callback(new Error('Not allowed by CORS'));
+    }
+}
+ 
+//全局解决跨域问题
+app.use(cors(corsOptions))
+
 // 挂载统一处理服务端错误中间件
 app.use(errorHandler());
 
 // 解析请求体
 app.use(express.json());
-// app.use(express.urlencoded());
-// TODO  为客户端提供跨域资源请求
-// app.use(cors());
 
 const metricsRouter = require("./router/metrics");
 app.use("/metrics", metricsRouter);
