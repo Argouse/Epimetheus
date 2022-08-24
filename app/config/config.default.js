4a1304bf88b79a3253b35201851558d3a1d29cb7
LinEvil <13027656+LinEvil@users.noreply.github.com>
2022-08-24 18:13:19 +0800

    remove prometheus from nodejs app

--- app/config/config.default.js
+++ app/config/config.default.js
@@ -1,10 +1,4 @@
 module.exports = {
-    // 监听端口号
-    // port: 80,
-    // pushgateway
-    pushgateway: {
-        url: 'http://127.0.0.1:9091'
-    },
     loki: {
         url: 'http://127.0.0.1:3100'
     }
--- app/controller/metrics.js
+++ app/controller/metrics.js
@@ -1,18 +1,6 @@
 const config = require('../config/config.js');
-const client = require('prom-client');
-const http = require('http');
 const winston = require('winston')
 const LokiTransport = require("winston-loki");
-
-const register = new client.Registry();
-let gateway = new client.Pushgateway(config.pushgateway.url, {
-  timeout: 5000, //Set the request timeout to 5000ms
-  agent: new http.Agent({
-    keepAlive: true,
-    keepAliveMsec: 10000,
-    maxSockets: 5,
-  }),
-}, register);
 
 const logger = winston.createLogger({
   format: winston.format.json(),
@@ -42,15 +30,7 @@ let metricsFunc = (funcName, funcParam) => {
 //       "time": "114514"
 //   }
 // }
-const gaugePageStay = new client.Gauge({
-  name: 'pageStay',
-  help: 'pageStayTime',
-  labelNames: ['pageurl'],
-  registers: [register]
-});
 pageStay = async (req, res, next) => {
-  gaugePageStay.labels({ pageurl: req.body.data.pageUrl }).set(Number(req.body.data.time));
-  gateway.pushAdd({ jobName: req.body.reportid }, (err, resp, body) => { });
   logger.info({ message: JSON.stringify(req.body.data), labels: {'type': 'pageStay' , 'reportid': req.body.reportid, 'pageurl': req.body.data.pageUrl } });
   res.json({ code: '200', message: 'success' });
 }
@@ -64,15 +44,7 @@ pageStay = async (req, res, next) => {
 //       "timeStamp": 1659854932187
 //   }
 // }
-const gaugeBlankScreenError = new client.Gauge({
-  name: 'blankScreenError',
-  help: 'blankScreenDOMNums',
-  labelNames: ['pageurl'],
-  registers: [register]
-});
 blankScreenError = async (req, res, next) => { // TODO
-  gaugeBlankScreenError.labels({ pageurl: req.body.data.pageUrl }).set(Number(req.body.data.whiteScreenDOMNums));
-  gateway.pushAdd({ jobName: req.body.reportid }, (err, resp, body) => { });
   logger.info({ message: JSON.stringify(req.body.data), labels: {'type': 'blankScreenError' , 'reportid': req.body.reportid, 'pageurl': req.body.data.pageUrl } });
   res.json({ code: '200', message: 'success' });
 }
@@ -90,20 +62,7 @@ blankScreenError = async (req, res, next) => { // TODO
 //       "requestTime": 1.199999999254942
 //   }
 // }
-const gaugePerformanceInfo = new client.Gauge({
-  name: 'pagePerformance',
-  help: 'pagePerformance Time',
-  labelNames: ['pageurl', 'type'],
-  registers: [register]
-});
 performanceInfo = async (req, res, next) => {
-  gaugePerformanceInfo.labels({pageurl: req.body.data.pageUrl, type: 'FP'}).set(Number(req.body.data.FP));
-  gaugePerformanceInfo.labels({pageurl: req.body.data.pageUrl, type: 'FCP'}).set(Number(req.body.data.FCP));
-  gaugePerformanceInfo.labels({pageurl: req.body.data.pageUrl, type: 'DNS'}).set(Number(req.body.data.DNS));
-  gaugePerformanceInfo.labels({pageurl: req.body.data.pageUrl, type: 'DOMready'}).set(Number(req.body.data.DOMready));
-  gaugePerformanceInfo.labels({pageurl: req.body.data.pageUrl, type: 'TCPduration'}).set(Number(req.body.data.TCPduration));
-  gaugePerformanceInfo.labels({pageurl: req.body.data.pageUrl, type: 'requestTime'}).set(Number(req.body.data.requestTime));
-  gateway.pushAdd({ jobName: req.body.reportid }, (err, resp, body) => { });
   logger.info({ message: JSON.stringify(req.body.data), labels: {'type': 'performanceInfo' , 'reportid': req.body.reportid, 'pageurl': req.body.data.pageUrl } });
   res.json({ code: '200', message: 'success' });
 }
